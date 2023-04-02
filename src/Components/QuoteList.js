43aac20690f4d77834850cfe685c36a40d258f8c
reshmalb <reshma.lb27@gmail.com>
2023-04-02 11:51:59 +0530

    first Commt

--- src/Components/QuoteList.js
+++ src/Components/QuoteList.js
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { Fragment } from "react";
 import QuoteItem from "./QuoteItem";
+import { useHistory,useLocation } from "react-router-dom";
+
+const sortQuotes=(quotes,ascending)=>{
+    return quotes.sort((quoteA,quoteB)=>{
+        if(ascending){
+            return quoteA.id > quoteB.id ? 1:-1;
+        }else{
+            return quoteA.id < quoteB.id ? 1:-1;
+        }
+    }) 
+};
+
+
 
 const QuoteList=(props)=>{
+const history=useHistory();
+const location=useLocation();
+const queryParams=new URLSearchParams(location.search)
+const isSortAscending=queryParams.get('sort')==='asc'
+const sortedQuotes=sortQuotes(props.quotes,isSortAscending)
+
+const changeSoringHandler=()=>{
+  history.push('/quotes?sort='+(isSortAscending ?'desc':'asc'))
+
+}
+
     return(
-        <div>
+        <Fragment>
+         <div>
+                <button  onClick={changeSoringHandler}>
+                 Sort {isSortAscending ? 'Descending' : 'Ascending'}
+                </button>
             <ul>
-            {props.quotes.map((quote)=>{
+            {sortedQuotes.map((quote)=>{
 
                return( <QuoteItem 
                     key={quote.id}
@@ -15,10 +43,13 @@ const QuoteList=(props)=>{
                 />)
             })}
             </ul>
+                  
+        </div>
+        </Fragment>
+    
       
 
-      
-        </div>
+
     )
 }
 
